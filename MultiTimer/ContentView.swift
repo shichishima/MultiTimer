6abd710616145f6f27716cd76cc8a59b95fa0cf4
shichishima <naoki@shichishima.jp>
2026-02-19 13:07:28 +0900

    Initial Commit

---
+++ MultiTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiTimer
+//
+//  Created by 七島直毅 on 2026/02/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiTimer/MultiTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTimerApp.swift
+//  MultiTimer
+//
+//  Created by 七島直毅 on 2026/02/19.
+//
+
+import SwiftUI
+
+@main
+struct MultiTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
